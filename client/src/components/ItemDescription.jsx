92af6610a18b54e38631e68a70f50a4e66757062
Hannah Holzmann <hholzmann16@gmail.com>
2018-09-13 11:08:16 -0700

    test 2

--- client/src/components/ItemDescription.jsx
+++ client/src/components/ItemDescription.jsx
@@ -23,9 +23,18 @@ class ItemDescription extends React.Component {
         </div>
         <div className={img.margin} />
         <div className={con.subContainer2}>
-          <span className={con.sub2name}>Tatcha</span>
+          <div className={con.sub2name}>Tatcha</div>
+          <div className={con.sub2description}>
+            Luminous Dewy Skin Night Concentrate
+          </div>
+          <div className={con.sub2size}>
+            SIZE 1.7 oz/ 50 mL {"\u00A0"}•{"\u00A0"} ITEM 1778851
+          </div>
         </div>
-        <div className={con.subContainer3} />
+        <div className={con.subContainer3}>
+          <div className={con.sub3price}>$104.36</div>
+          <div className={con.sub3ship}>Free Flash Shipping</div>
+        </div>
         <div className={con.subContainer4} />
         <div className={con.subContainer5} />
       </div>
