b015a31fdc4111fe5c7a2e193694248f6b544354
Hannah Holzmann <hholzmann16@gmail.com>
2018-09-18 20:02:04 -0700

    unbroke things

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import ItemDescription from "./components/ItemDescription.jsx";
 
-ReactDOM.render(<ItemDescription />, document.getElementById("app"));
+ReactDOM.render(<ItemDescription />, document.getElementById("hannah"));
