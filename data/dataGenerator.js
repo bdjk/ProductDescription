f2bc7ebbc8de2df61bb457f53c1a36d824447190
jbweee <bui.just@gmail.com>
2018-10-01 15:30:28 -0700

    Trying other drain methods

--- data/dataGenerator.js
+++ data/dataGenerator.js
@@ -13,6 +13,8 @@ var stream = fs.createWriteStream(tsvPath, {'flags': 'a'});
 
 var i = 0;
 
+//330sec
+//This makes the most sense to me as opposed to setting flags
 function write() {
   while (i < 10000000) {
     let entry = [
@@ -25,6 +27,7 @@ function write() {
       `https://picsum.photos/300/300/?image=${Math.ceil(Math.random() * 1084)}`
     ]
     entry = entry.join('\t');
+    // console.log(!stream.write(entry + '\n', i++))
     if (!stream.write(entry + '\n', i++)) {
       return;
     }
@@ -39,6 +42,45 @@ stream.on('drain', function() {
   write();
 });
 write();
+
+//299sec
+// write();
+// function write() {
+//   let ok = true;
+//   do {
+//     let entry = [
+//       i, 
+//       faker.company.companyName(),
+//       faker.commerce.productName(),
+//       `$${faker.finance.amount()}`,
+//       (Math.random() * 5).toFixed(1),
+//       Math.floor(Math.random() * 1000),
+//       `https://picsum.photos/300/300/?image=${Math.ceil(Math.random() * 1084)}`
+//     ]
+//     entry = entry.join('\t');
+//     // console.log(!stream.write(entry + '\n', i++))
+//     // if (!stream.write(entry + '\n', i++)) {
+//     //   return;
+//     // }
+//     i++;
+//     if (i % 1000000 === 0) {
+//       console.log(`Wrote ${i} records to file`);
+//     } 
+//     if (i === 10000000) {
+//       stream.write(entry + '\n')
+//       stream.end();
+//     } else {
+//       ok = stream.write(entry + '\n')
+//     }
+//   } while(i < 10000000 && ok); 
+//   if (i < 10000000) {
+//     // had to stop early!
+//     // write some more once it drains
+//     stream.once('drain', () => {
+//       write()
+//     });
+//   }
+// }
 
 stream.on('finish', () => {
   var end = now();
